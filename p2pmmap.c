1125fd47a1f7ae68de868d5b713aa52018681b0d
Ryo Nakamura <upa@haeena.net>
2019-03-01 21:56:19 +0900

    add mmap ops

--- p2pmmap.c
+++ p2pmmap.c
@@ -23,13 +23,97 @@ module_param(p2pmem_size, uint, 0);
 MODULE_PARM_DESC(p2pmem_size, "size of allocating p2pmem");
 
 struct p2pmmap {
-	struct pci_dev	*pdev;	/* target pci dev with p2pmem	*/
-	void	*p2pmem;	/* p2pmem of the above pci dev	*/
-	size_t	size;		/* p2pmem size */
+	struct pci_dev	*pdev;		/* target pci dev with p2pmem	*/
+	void		*p2pmem;	/* p2pmem of the above pci dev	*/
+	size_t		size;		/* p2pmem size */
+	atomic_t	opened;		/* opened */
 };
 
 static struct p2pmmap pmm;	/* describing this module */
 
+
+
+static int p2pmmap_open(struct inode *inode, struct file *filp)
+{
+	if (atomic_read(&pmm.opened))
+		return -EBUSY;
+
+	atomic_inc(&pmm.opened);
+	return 0;
+}
+
+static int p2pmmap_release(struct inode *inode, struct file *filp)
+{
+	atomic_dec(&pmm.opened);
+	return 0;
+}
+
+static int p2pmmap_mem_fault(struct vm_fault *vmf)
+{
+	struct vm_area_struct *vma = vmf->vma;
+	struct page *page;
+	unsigned long pagenum = vmf->pgoff;
+	unsigned long pa, pfn;
+
+	pr_info("%s: vma->vm_pgoff=%ld, vmf->pgoff=%ld\n",
+		__func__, vma->vm_pgoff, vmf->pgoff);
+	pr_info("%s: page number %ld\n", __func__, pagenum);
+
+
+	pa = virt_to_phys(pmm.p2pmem + (pagenum << PAGE_SHIFT));
+	pr_info("%s: paddr of mapped p2pmem is %lx\n",
+		__func__, pa);
+	if (pa == 0) {
+		pr_err("wrong pa\n");
+		return VM_FAULT_SIGBUS;
+	}
+
+	pfn = pa >> PAGE_SHIFT;
+	if (!pfn_valid(pfn)) {
+		pr_err("invalid pfn %lx\n", pfn);
+		return VM_FAULT_SIGBUS;
+	}
+
+	page = pfn_to_page(pfn);
+	get_page(page);
+	vmf->page = page;
+
+	return 0;
+}
+
+static struct vm_operations_struct p2pmmap_mmap_ops = {
+	.fault	= p2pmmap_mem_fault,
+};
+
+static int p2pmmap_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	unsigned long off = vma->vm_pgoff << PAGE_SHIFT;
+	unsigned long len = vma->vm_end - vma->vm_start;
+
+	pr_debug("%s: offset is %lu, length is %lu\n", __func__, off, len);
+	if (off + len > pmm.size) {
+		pr_err("%s: len %lu is larger than p2pmem size %lu\n",
+		       __func__, len,  pmm.size);
+		return -ENOMEM;
+	}
+
+	vma->vm_ops = &p2pmmap_mmap_ops;
+	return 0;
+}
+
+
+static struct file_operations p2pmmap_fops = {
+	.owner		= THIS_MODULE,
+	.mmap		= p2pmmap_mmap,
+	.open		= p2pmmap_open,
+	.release	= p2pmmap_release,
+};
+
+static struct miscdevice p2pmmap_dev = {
+	.minor	= MISC_DYNAMIC_MINOR,
+	.name	= "p2pmmap",
+	.fops	= &p2pmmap_fops,
+};
 
 
 static int __init p2pmmap_init(void)
@@ -69,12 +153,26 @@ static int __init p2pmmap_init(void)
 		return -ENOTSUPP;
 	}
 
+	if (p2pmem_size < (1 << PAGE_SHIFT) ||
+	    p2pmem_size % (1 << PAGE_SHIFT) != 0) {
+		pr_err("p2pmem_size must be power of %d\n", 1 << PAGE_SHIFT);
+		goto err_p2pmem_alloc_failed;
+	}
+
+	atomic_set(&pmm.opened, 0);
 	pmm.pdev = pdev;
 	pmm.size = p2pmem_size;
 	pmm.p2pmem = pci_alloc_p2pmem(pdev, pmm.size);
 	if (!pmm.p2pmem) {
 		pr_err("failed to allocate %lu-byte p2pmem\n", pmm.size);
 		goto err_p2pmem_alloc_failed;
+	}
+
+	/* register miscdevice */
+	ret = misc_register(&p2pmmap_dev);
+	if (ret) {
+		pr_err("failed to register miscdevice for p2pmmap\n");
+		goto err_misc_register_failed;
 	}
 
 	pr_info("p2pmmap (v%s) is loaded.\n", P2PMMAP_VERSION);
@@ -88,12 +186,15 @@ err_no_pci_slot:
 	return -EINVAL;
 
 err_p2pmem_alloc_failed:
+	ret = -EINVAL;
+err_misc_register_failed:
 	pci_dev_put(pmm.pdev);
-	return -EINVAL;
+	return ret;
 }
 
 static void __exit p2pmmap_exit(void)
 {
+	misc_deregister(&p2pmmap_dev);
 	pci_free_p2pmem(pmm.pdev, pmm.p2pmem, pmm.size);
 	pci_dev_put(pmm.pdev);
 
